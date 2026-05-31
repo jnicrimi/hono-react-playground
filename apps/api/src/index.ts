7224acaf9dbf07026f0b043d88e0490461526f18
Junichiro Imai <jnicrimi@gmail.com>
2026-05-31 17:50:40 +0900

    feat: add Hono api app

---
+++ apps/api/src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hello, World!"));
+
+const server = serve(app);
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
